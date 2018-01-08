96311075b418c03d24c64d28407c91799645e455
brendamesias <brendamesiasvillar@gmail.com>
2018-01-08 18:30:12 -0500

    creando documento js

---
+++ js/interests.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('selector').evento();
+
+//crear evento que con un togle cambie  el fondo de llas optciones de intereses por parte del susuraio
+
+
+
+
+
+
+
+});
