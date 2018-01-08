3d3054829fb00adeffc4c55bbed839ea13fa3a41
SteffAhv <tefavv22@gmail.com>
2018-01-08 13:44:48 -0500

    añade vista splash,vista welcome

---
+++ js/app.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', function() {
+
+	// Redireccionando a la vista home
+
+	// setTimeout(function() {
+	// 	window.location.href = 'views/welcome.html';
+	// }, 8000);
+	
+
+	// // Initialize Firebase
+	// var config = {
+	// 	apiKey: "AIzaSyCAqTCcsvkHHN8z3Qa85cI9Sd4CKwmjHA8",
+	// 	authDomain: "connectech-1a62a.firebaseapp.com",
+	// 	databaseURL: "https://connectech-1a62a.firebaseio.com",
+	// 	projectId: "connectech-1a62a",
+	// 	storageBucket: "",
+	// 	messagingSenderId: "822294911375"
+	// };
+	// firebase.initializeApp(config);
+
+	// firebase.auth(): Authentication;
+	// firebase.storage(): Cloud Storage;
+	// firebase.database(): Realtime Database;
+});
