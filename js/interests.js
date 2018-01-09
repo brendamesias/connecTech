e1cc65f492698d0cb09a4b820532f1cc933fde71
brendamesias <brendamesiasvillar@gmail.com>
2018-01-09 10:44:51 -0500

    agregando funcionalidad

--- js/interests.js
+++ js/interests.js
@@ -1,12 +1,7 @@
 $(document).ready(function() {
-  $('selector').evento();
-
-//crear evento que con un togle cambie  el fondo de llas optciones de intereses por parte del susuraio
-
-
-
-
-
+  $('.option_interests').click(function() {
+    $('.option_interests').toggle(.addClass('selected_interest'));
+  });
 
 
 });
