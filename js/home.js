fb76bf7c366bd76b6ed8fef4faa63ff39d158c33
SteffAhv <tefavv22@gmail.com>
2018-01-12 12:36:18 -0500

    añade publicaciones

---
+++ js/home.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    // variables del dom declaradas (modal)
+    var tagsForPost = $('#tag')[0];
+    console.log(tagsForPost);
+  
+    var valorInputTag = $('#tag').val();
+    console.log(valorInputTag);
+  
+    var optionsTagPost = $('#tag').children();
+    console.log(optionsTagPost);
+  
+    var typePost = $('#type_post')[0];
+    console.log(typePost);
+  
+    var valorInputTypePost = $('#type_post').val();
+    console.log(valorInputTypePost);
+  
+    var optionsTypePost = $('#type_post').children();
+    console.log(optionsTypePost);
+  
+    var inputTitlePostVal = $('.title-post-input');
+    var descriptionPostVal = $('.description-post');
+  
+    // trayenpo del dom (estructura de una publicaciopn contenido)
+    var titlePost = $('#title_post').text();
+    // console.log(titlePost); --> Nuevo Lenguaje de programación cuántico
+    var descriptionPost = $('#description_post').text();
+  
+    var btnSubmitFormPost = $('#submit_form_post');
+    var containerPublications = document.getElementById('all_publications');
+    console.log(containerPublications);
+    var structurePublication = document.getElementById('publication');
+    console.log(structurePublication);
+    // console.log(structurePublication.children());
+  
+    // funciones que capturen las entradas de los inputs y formularios
+  
+    $(tagsForPost).click(function(event) {
+      valorInputTag = $(this).val();
+      console.log(valorInputTag);
+    });
+  
+    $(typePost).click(function(event) {
+      valorInputTypePost = $(this).val();
+      console.log(valorInputTypePost);
+    });
+  
+    inputTitlePostVal.on('input', function() {
+      inputTitlePostVal = event.target.value ;
+      // console.log('newtitle:  ' + inputTitlePostVal);
+    });
+  
+    descriptionPostVal.on('input', function() {
+      inputDescriptionPostVal = event.target.value ;
+      // console.log('newdescription :  ' + descriptionPostVal);
+    });
+  
+    btnSubmitFormPost.click(function(event) {
+      var clonePost = structurePublication.cloneNode(true);
+      console.log(clonePost);
+      clonePost.children[0].children[0].children[0].children[1].children[0].children[0].children[0].firstElementChild.textContent = inputTitlePostVal ;
+      clonePost.children[0].children[0].children[0].children[1].children[0].children[0].children[1].children[0].textContent = inputDescriptionPostVal ;
+      clonePost.classList.remove('display_none');
+      containerPublications.prepend(clonePost);
+    });
+  });
